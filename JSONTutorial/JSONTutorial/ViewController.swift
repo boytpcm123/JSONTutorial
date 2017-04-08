11016cb5345e0cda0453095ffb9728b68cd99bb0
ninjaKID <ninjaKID@Macintoshs-MacBook-Pro-2.local>
2017-04-08 17:51:31 +0700

    init project

---
+++ JSONTutorial/JSONTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONTutorial
+//
+//  Created by ninjaKID on 4/8/17.
+//  Copyright © 2017 ninjaKID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
