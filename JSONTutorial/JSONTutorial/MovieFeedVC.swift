57104fc5b7339602654ba9613e81215a175cbffc
ninjaKID <ninjaKID@Macintoshs-MacBook-Pro-2.local>
2017-04-08 18:14:02 +0700

    Init table view

--- JSONTutorial/JSONTutorial/MovieFeedVC.swift
+++ JSONTutorial/JSONTutorial/MovieFeedVC.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class MovieFeedVC: UIViewController {
+class MovieFeedVC: UITableViewController {
+    
+    private let cellID = "cellID"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID)!
+        return cell
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100;
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
